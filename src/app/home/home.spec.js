ea2b037252eebdb3cd85707ba9f027a5a024f170
Connor Simmons <connorlsimmons@gmail.com>
2014-08-02 19:56:30 -0500

    Initial commit of updated ng-boilerplate clone

---
+++ src/app/home/home.spec.js
@@ -0,0 +1,8 @@
+describe( 'home section', function() {
+  beforeEach( module( 'ngBoilerplate.home' ) );
+
+  it( 'should have a dummy test', inject( function() {
+    expect( true ).toBeTruthy();
+  }));
+});
+
